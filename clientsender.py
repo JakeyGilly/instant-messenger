a5a206a388db0ab9ccb11e7a20767707dc82285e
Finn O'Neill <56732347+Explorer017@users.noreply.github.com>
2020-06-12 18:18:17 +0100

    Delete clientsender.py

--- clientsender.py
+++
@@ -1,8 +0,0 @@
-import socket
-name = input("Please enter your name: ")
-clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientsocket.connect(("", 1234))
-while 1:
-    message = input("> ")
-    clientsocket.send(name.encode() + ": ".encode() + message.encode())
-clientsocket.close()

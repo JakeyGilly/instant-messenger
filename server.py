265c8df6faae8d234b313337ba5fbde7fa59fa73
Finn O'Neill <56732347+Explorer017@users.noreply.github.com>
2020-06-14 15:59:11 +0100

    Fixed "Broken Pipe" issue

--- server.py
+++ server.py
@@ -1,15 +1,36 @@
-import socket,_thread
+import socket,_thread,threading
+
+
+clients = []
 
 def recv(clientsocket,addr):
     while True:
         msg = clientsocket.recv(1024)
-        print(msg.decode())
+        ah = msg.decode()
+        epic = ah.split(",")
+        if epic[0] == "Disconnect#000000":
+            clients.remove(clientsocket)
+            print(f'[CONNECT] Disconnected  {addr}')
+        else:
+            print(f"[MESSAGE] Message received: {epic[0]} saying {epic[1]}")
+        for i in range(len(clients)):
+            client = clients[i]
+            try:
+             client.sendall(ah.encode())
+            except BrokenPipeError:
+                clients.remove(clients[i])
+                break
+
     clientsocket.close()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-IP = ""
-PORT = 1234
+host_name = socket.gethostname()
+IP = socket.gethostbyname(host_name)
+PORT = int(input("port? "))
 
+
+print("           Thanks for choosing PyChat!")
+print("               PyChat Server V0.1")
 print("                Server started!")
 print("------------------------------------------------")
 print(f"Listening for connections on {IP}:{PORT}...")
@@ -20,7 +41,8 @@ while True:
     s.listen(5)
     c, addr = s.accept()
     _thread.start_new_thread(recv, (c, addr))
-    print(f'Accepted new connection from {addr}')
+    print(f'[CONNECT] Accepted new connection from {addr}')
+    clients.append(c)
 
 
 

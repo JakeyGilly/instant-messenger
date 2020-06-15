4e9f61e11f7b8525a5db43281eb95c587062edeb
Finn O'Neill <56732347+Explorer017@users.noreply.github.com>
2020-06-15 12:00:46 +0100

    Updated from master

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-import socket,_thread,threading
+import socket,_thread,threading,datetime
+now = datetime.datetime.now()
 
 
 clients = []
@@ -10,27 +11,53 @@ def recv(clientsocket,addr):
         epic = ah.split(",")
         if epic[0] == "Disconnect#000000":
             clients.remove(clientsocket)
-            print(f'[CONNECT] Disconnected  {addr}')
+            print(f'[{now.strftime("%Y-%m-%d %H:%M:%S")}][DISCONNECT] Disconnected  {addr}')
+            with open('log.csv', mode='a') as log:
+                log.writelines("\n" + str([{now.strftime("%Y-%m-%d %H:%M:%S")} ,"[DIS]", addr, "N/A", "N/A"]))
+        elif epic[0] == "INFO#000000":
+             ah = f"[SERVER]#000000,{epic[1]} has joined the chat!"
+             with open('log.csv', mode='a') as log:
+                log.writelines("\n" + str([{now.strftime("%Y-%m-%d %H:%M:%S")} ,"[CON]", addr, epic[1], "N/A"]))
         else:
-            print(f"[MESSAGE] Message received: {epic[0]} saying {epic[1]}")
+            print(f'[{now.strftime("%Y-%m-%d %H:%M:%S")}][MESSAGE] Message received: {epic[0]} saying {epic[1]}')
+        with open('log.csv', mode='a') as log:
+            log.writelines("\n" + str([{now.strftime("%Y-%m-%d %H:%M:%S")} , "[MSG]" ,addr ,  epic[0], epic[1]]))
         for i in range(len(clients)):
             client = clients[i]
             try:
              client.sendall(ah.encode())
             except BrokenPipeError:
                 clients.remove(clients[i])
+                print(f'[{now.strftime("%Y-%m-%d %H:%M:%S")}][IDC] Illegal Disconnect (Check Log)')
+                with open('log.csv', mode='a') as log:
+                    log.writelines("\n" + str([{now.strftime("%Y-%m-%d %H:%M:%S")} ,"[IDC]", "N/A","N/A",client]))
                 break
+
 
     clientsocket.close()
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host_name = socket.gethostname()
 IP = socket.gethostbyname(host_name)
+ipcorrect = input(f"Is your private IP: {IP}? [Y/N]")
+answered = False
+
+while not answered:
+    if ipcorrect.lower() == "y":
+        IP = IP
+        answered = True
+    elif ipcorrect.lower() == "n":
+        IP = input("What is your private IP")
+        answered = True
+    else:
+        print("Please enter Y/N")
+
+
 PORT = int(input("port? "))
 
 
 print("           Thanks for choosing PyChat!")
-print("               PyChat Server V0.1")
+print("              PyChat Server V0.1.1")
 print("                Server started!")
 print("------------------------------------------------")
 print(f"Listening for connections on {IP}:{PORT}...")
@@ -41,7 +68,8 @@ while True:
     s.listen(5)
     c, addr = s.accept()
     _thread.start_new_thread(recv, (c, addr))
-    print(f'[CONNECT] Accepted new connection from {addr}')
+    print(f'[{now.strftime("%Y-%m-%d %H:%M:%S")}][CONNECT] Accepted new connection from {addr}')
+
     clients.append(c)
 
 

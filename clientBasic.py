eb0b941bb0a9ed6728376c56bf7fc2cff4ad41eb
Finn O'Neill <56732347+Explorer017@users.noreply.github.com>
2020-06-18 13:06:54 +0100

    Uploaded it

---
+++ clientBasic.py
@@ -0,0 +1,37 @@
+import threading
+import socket
+import time as t
+import random as r
+
+name = input("Please enter your name: ")
+name = name + "#" + str(r.randint(100000,999999))
+clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+clientsocket.connect((input("IP of server? (eg: 127.0.0.1)"), int(input("Port of server? (eg: 1111)"))))
+clientsocket.setblocking(True)
+clientsocket.send(f"INFO#000000,{name}".encode())
+print("     		      PyChat 0.1")
+print("                Connected to Server!")
+print(f"			Type /exit to disconnect!")
+print("------------------------------------------------")
+
+def get_input():
+		while True:
+			msg = clientsocket.recv(1024)
+			ah = msg.decode()
+			epic = ah.split(",")
+			if epic[0] != name:
+				print(f"\n{epic[0][:-7]}) {epic[1]}")
+
+
+input_thread = threading.Thread(target=get_input)
+input_thread.start()
+
+# Run your game now
+while True:
+	message = input(f"")
+	toSend = name + "," + message
+	if message == "/exit":
+		toSend = f"Disconnect#000000,{name} Has disconnected!"
+		clientsocket.send(toSend.encode())
+		clientsocket.close()
+	clientsocket.send(toSend.encode())
